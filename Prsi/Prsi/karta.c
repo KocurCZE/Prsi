17d46710daab1bf75d0ff1579d4fb4ef8d551a56
Jakub Kočka <KocurCZE@gmail.com>
2018-12-21 23:35:11 +0100

    hmm

--- Prsi/Prsi/karta.c
+++ Prsi/Prsi/karta.c
@@ -1,10 +1,10 @@
 // obsahuje pouze implementace (definici) tìchto funkcí ... karta.h
 #include "karta.h"
 
-vytvorKartu(enum tBarvaKarty barvaKarty, enum tTypKarty typKarty, char *nazev) {
-	 //barvaKarty = Vubec nic jsem nenapsal
+tKarta * vytvorKartu(enum tBarvaKarty barvaKarty, enum tTypKarty typKarty, char *nazev) {
+	tBarvaKarty barvaKarty = rand(7);
 }
 
-vypisKartu(tKarta *karta) {
-	printf("")
+void vypisKartu(tKarta *karta) {
+	printf("");
 }
